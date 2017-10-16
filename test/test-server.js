026f5e7b434d867d47a43d1990e1232e92eaff85
Brian Newell <brian.newell@gmail.com>
2017-10-16 08:33:55 -0700

    fixed tests

--- test/test-server.js
+++ test/test-server.js
@@ -7,111 +7,89 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('GET endpoint', function() {
-  	before(function() {
-    	return runServer();
+describe('tests', function() {
+	before(function() {
+		return runServer();
   	});
 
   	after(function() {
-    	return closeServer();
+		return closeServer();
   	});
-	it('should list terms on GET', function() {
-	return chai.request(app)
-		.get('/terms')
-		.then(function(res) {
-			res.should.have.status(200);
-			res.should.be.json;
-			res.should.be.a('object');
-			res.body.terms.should.have.length.of.at.least(1);
-			const expectedKeys = ['id', 'year', 'english', 'lao'];
-			'terms'.should.include.keys(expectedKeys);
-			// const expectedKeys = ['year', 'english', 'lao'];
-			// res.body.forEach(function(terms) {
-			// 	terms.should.be.a('object');
-			// 	terms.should.include.keys(expectedKeys);
-			// });
+
+	describe('GET endpoint', function() {
+		it('should return terms', function() {
+			return chai.request(app)
+				.get('/terms')
+				.then(function(res) {
+					res.should.have.status(200);
+					res.should.be.json;
+					res.should.be.a('object');
+					res.body.terms.should.have.length.of.at.least(4);
+					res.body.terms.forEach(function(term) {
+						term.should.be.a('object');
+						term.should.include.keys('id', 'year', 'english', 'lao');
+					});
+				});
+		});
+	});
+
+	describe('POST endpoint', function() {
+		it('should add term on POST', function() {
+		const newTerm = {year: "1", type: "root", english: "cardio", lao: "ພາສາ ລາວ" };
+		return chai.request(app)
+			.post('/terms')
+			.send(newTerm)
+			.then(function(res) {
+				res.should.have.status(201);
+				res.should.be.json;
+				res.should.be.a('object');
+				res.body.should.include.keys('year', 'type', 'english', 'lao');
+				res.body.year.should.equal(newTerm.year);
+				res.body.type.should.equal(newTerm.type);
+				res.body.english.should.equal(newTerm.english);
+				res.body.lao.should.equal(newTerm.lao);
+			});
+		});
+	});
+
+	describe('PUT endpoint', function() {
+
+		it('should update term on PUT', function() {
+
+			const newTerm = {year: "1", type: "root", english: "cardio", lao: "ພາສາ ລາວ" };
+			return chai.request(app)
+				.post('/terms')
+				.send(newTerm)
+				.then(function(res) {
+					return chai.request(app)
+					.put(`/terms/${res.body.id}`)
+					.send({id: res.body.id, year: "3"});
+
+				})
+				.then(function(res) {
+					res.should.have.status(204);
+				});
+
+
+		});
+	});
+
+	describe('DELETE endpoint', function() {
+		it('should delete terms on DELETE', function() {
+			const newTerm = {year: "1", type: "root", english: "cardio", lao: "ພາສາ ລາວ" };
+			return chai.request(app)
+				.post('/terms')
+				.send(newTerm)
+				.then(function(res) {
+					return chai.request(app)
+						.delete(`/terms/${res.body.id}`);
+				})
+				.then(function(res) {
+					res.should.have.status(204);
+				});
+			
 		});
 	});
 });
 
-describe('POST endpoint', function() {
-	before(function() {
-    	return runServer();
-  	});
-
-  	after(function() {
-    	return closeServer();
-  	});
-	it('should add term on POST', function() {
-	const newTerm = {year: "1", type: "root", english: "cardio", lao: "ພາສາ ລາວ" };
-	return chai.request(app)
-		.post('/terms')
-		.send(newTerm)
-		.then(function(res) {
-			res.should.have.status(201);
-			res.should.be.json;
-			res.should.be.a('object');
-			res.body.should.include.keys('year', 'type', 'english', 'lao');
-			// res.body.id.should.not.be.null;
-			res.body.year.should.equal(newTerm.year);
-			res.body.type.should.equal(newTerm.type);
-			res.body.english.should.equal(newTerm.english);
-			res.body.lao.should.equal(newTerm.lao);
-			// res.body.should.deep.equal(Object.assign(newTerm, {id: res.body.id}));
-		});
-	});
-});
-
-describe('PUT endpoint', function() {
-	before(function() {
-    	return runServer();
-  	});
-
-  	after(function() {
-    	return closeServer();
-  	});
-	it('should update term on PUT'), function() {
-		const updateTerm = {year: "3", type: "root", english: "cardi/o", lao: "ພາສາ ລາວ" };
-		return chai.request(app)
-			.get('/terms')
-			.then(function(res) {
-				updateTerm.year = res.body[0].id;//array?
-				return chai.request(app)
-					.put(`/terms/${updateTerm.year}`)
- 					.send(updateTerm)
-			})
-			.then(function(res) {
-				res.should.have.status(204);
-			});
-	};
-});
-
-describe('DELETE endpoint', function() {
-	before(function() {
-    	return runServer();
-  	});
-
-  	after(function() {
-    	return closeServer();
-  	});
-	it('should delete terms on DELETE', function() {
-		return chai.request(app)
-			.get('/terms')
-			.then(function(res) {
-				return chai.request(res) 
-					.delete(`/terms/${res.body[0].id}`);
-			})
-			.then(function(res) {
-				res.should.have.status(204);
-			});
-	});
-});
-
-
-// db.terms.insert([
-// 	{ year : "3", type : "root", english : "abdomin/o", lao : "ພາສາ ລາວ" },
-// 	{ year : "3", type : "root", english : "bio", lao : "ພາສາ ລາວ" },
-// 	{ year : "3", type : "root", english : "aden/o", lao : "ພາສາ ລາວ" },
-// 	{ year : "3", type : "root", english : "anter/o", lao : "ພາສາ ລາວ" }
-// ])
 
